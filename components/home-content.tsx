6c6d74c5390933d68ccc690932f853e4fc7733fd
Denizbukut <denizbukut90@gmail.com>
2025-12-21 14:52:26 +0100

    card creations

--- components/home-content.tsx
+++ components/home-content.tsx
@@ -1881,7 +1881,7 @@ export default function Home() {
             >
               <div className="flex items-center gap-2">
                 <Sparkles className="h-4 w-4 text-black" />
-                <span>{t("home.create_card_earn", "Create Your Own Card and Earn")}</span>
+                <span>{t("home.create_card_earn", "25% discount on card creations")}</span>
                 <ArrowRight className="ml-auto h-4 w-4 text-black/80" />
               </div>
             </motion.div>
